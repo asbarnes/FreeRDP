08973ae099253d537680547eb10e621545ad15f2
Assaf Bar Ness <88961125+asbarnes@users.noreply.github.com>
2022-12-12 13:19:35 +0200

    debug print

--- client/X11/xf_event.c
+++ client/X11/xf_event.c
@@ -477,7 +477,7 @@ static BOOL xf_event_KeyPress(xfContext* xfc, const XKeyEvent* event, BOOL app)
 	WINPR_UNUSED(app);
 	printf("xf_event_KeyPress\n");
 	XLookupString((XKeyEvent*)event, str, sizeof(str), &keysym, NULL);
-	printf("xf_event_KeyPress after: %s\n", str);
+	printf("xf_event_KeyPress after: %s %d\n", str, keysym);
 	xf_keyboard_key_press(xfc, event->keycode, keysym);
 	printf("---> %d\n", event->keycode);
 	return TRUE;
